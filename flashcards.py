ecbdd6bcad0dff3819b1743dc37ba2af502c76ef
carhawk-06 <carhawk06@gmail.com>
2026-09-02 15:27:42 -0400

    v2

--- flashcards.py
+++ flashcards.py
@@ -2,24 +2,34 @@ import random
 
 
 
-cards = [{"term": "list", "definition": "an ordered, changeable sequence"},{"term": "dict", "definition": "key-value pairs looked up by key"},{"term": "set", "definition": "unordered, no duplicates"},{"term": "tuple", "definition": "a list, but immutable"}]
+cards = [{"term": "list", "definition": "an ordered, changeable sequence"},
+         {"term": "dict", "definition": "key-value pairs looked up by key"},
+         {"term": "set", "definition": "unordered, no duplicates"},
+         {"term": "tuple", "definition": "a list, but immutable"}]
 
 
-nocards = False
 if not cards:
-    print ("No cards in deck")
-    nocards = True
+    print("No cards in deck")
 else:
     print("There are", len(cards) ,"cards in the deck")
-    print(cards[-1]["term"], "->", cards[-1]["definition"])
+    score = 0
+    cards_reviewed = []
+    for card in range(len(cards)):
+        shuffled_deck = random.sample(cards, k=len(cards))
+        card = random.choice(shuffled_deck)
 
-    card = random.choice(cards)
+        while card["term"] in cards_reviewed:
+            card = random.choice(shuffled_deck)
 
-    print("The term is:", card["term"])
-    if input("What is your answer?: ") == card["definition"]:
-        print("You got it right")
-    else:
-        print("You got it wrong, the correct answer was", card["definition"])
+        print("The definition is:", card["definition"])
+        if input("What is your answer?: ") == card["term"]:
+            print("You got it right")
+            score += 1
+        else:
+            print("You got it wrong, the correct answer was", card["term"])
 
+        cards_reviewed.append(card["term"])
 
+    print("Done! You got:", score, "right out of", len(cards))
 
+    
